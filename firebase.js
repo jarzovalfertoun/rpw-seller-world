dd71e5b5624b0db76335c46aa6cea8c756cb5d21
jarzovalfertoun <nianmaranadelumennn@gmail.com>
2026-09-17 17:28:30 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -35,18 +35,18 @@ import {
 
 const firebaseConfig = {
 
-    apiKey: "PASTE_YOUR_API_KEY",
+    apiKey: "AIzaSyCf1_ldd1PxAPgcRH_vd6L5oXXtrRSMjpk",
 
-    authDomain: "PASTE_YOUR_AUTH_DOMAIN",
+    authDomain: "rpw-seller-world-ccb3a.firebaseapp.com",
 
-    projectId: "PASTE_YOUR_PROJECT_ID",
+    projectId: "rpw-seller-world-ccb3a",
 
-    storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
+    storageBucket: "rpw-seller-world-ccb3a.firebasestorage.app",
 
     messagingSenderId:
-        "PASTE_YOUR_MESSAGING_SENDER_ID",
+        "645033393554",
 
-    appId: "PASTE_YOUR_APP_ID"
+    appId: "1:645033393554:web:30a2a7fa26ada1ab26d54b"
 
 };
 
